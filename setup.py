29ab57dee075b7a48a81eab6d087f29ab566283b
Carlos Uribe <curibe@bccrc.ca>
2021-09-21 17:18:13 -0700

    add openpyxl as install requirement

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(name='DOODLE',
       packages=['doodle','doodle.tests'],
       include_package_data=True,
       install_requires = [
-          'numpy','matplotlib','pandas','pydicom'
+          'numpy','matplotlib','pandas','pydicom','openpyxl'
       ],
       zip_safe=False)
