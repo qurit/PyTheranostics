5233a4dd16bab4aeddef3ab89e3dc318f46a3ba6
Carlos Uribe <curibe@bccrc.ca>
2022-06-24 10:02:57 -0700

    Add plots for mono and biexponential fits

--- doodle/plots/plots.py
+++ doodle/plots/plots.py
@@ -28,3 +28,91 @@ def ewin_montage(img,ewin):
 
 
     plt.tight_layout()
+
+
+
+
+
+def monoexp_fit_plots(t,a,tt,yy,organ,r_squared,residuals,skip_points=0,sigmas=None,**kwargs):
+    fig, axes = plt.subplots(1, 3, figsize=(10,4))
+
+    if sigmas:
+        axes[0].errorbar(t,a,yerr=sigmas,fmt='o')
+        # axes[1,0].errorbar(t,a,yerr=sigmas,fmt='o')
+    else:
+        axes[0].plot(t,a,'o',color='#1f77b4',markeredgecolor='black')
+
+    if skip_points:
+        axes[0].plot(tt[tt>=t[skip_points]],yy)
+        axes[1].semilogy(tt[tt>=t[skip_points]],yy)
+    else:
+        axes[0].plot(tt,yy)
+        axes[1].semilogy(tt,yy)
+
+    axes[0].set_title('{} Mono-Exponential'.format(organ))
+    axes[0].set_xlabel('t (days)')
+    axes[0].set_ylabel('A (MBq)')
+    axes[0].text(0.6,0.85,'$R^2={:.4f}$'.format(r_squared),transform=axes[0].transAxes)
+
+    axes[1].semilogy(t[skip_points:],a[skip_points:],'o',color='#1f77b4',markeredgecolor='black')
+    axes[1].set_xlabel('t (days)')
+    axes[1].set_ylabel('A (MBq)')
+    axes[1].set_title('{} Mono-Exponential'.format(organ))
+
+    axes[2].plot(t[skip_points:],residuals,'o')
+    axes[2].set_title('Residuals')
+    axes[2].set_xlabel('t (days)')
+    axes[2].set_ylabel('A (MBq)')
+
+    plt.tight_layout()
+
+    if 'save_path' in kwargs:
+        plt.savefig(f"{kwargs['save_path']}/{organ}_monoexp_fit.png",dpi=300)
+
+
+
+def biexp_fit_plots(t,a,tt,yy,organ,r_squared,residuals,skip_points=0,sigmas=None,**kwargs):
+    fig, axes = plt.subplots(1, 3, figsize=(10,4))
+
+    if sigmas:
+        axes[0].errorbar(t,a,yerr=sigmas,fmt='o')
+        # axes[1,0].errorbar(t,a,yerr=sigmas,fmt='o')
+    else:
+        axes[0].plot(t,a,'o',color='#1f77b4',markeredgecolor='black')
+
+    if skip_points:
+        axes[0].plot(tt[tt>=t[skip_points]],yy)
+        axes[1].semilogy(tt[tt>=t[skip_points]],yy)
+    else:
+        axes[0].plot(tt,yy)
+        axes[1].semilogy(tt,yy)
+
+    axes[0].set_title('{} Bi-Exponential'.format(organ))
+    axes[0].set_xlabel('t (days)')
+    axes[0].set_ylabel('A (MBq)')
+    axes[0].text(0.6,0.85,'$R^2={:.4f}$'.format(r_squared),transform=axes[0].transAxes)
+    
+
+
+    axes[1].semilogy(t[skip_points:],a[skip_points:],'o',color='#1f77b4',markeredgecolor='black')
+    axes[1].set_xlabel('t (days)')
+    axes[1].set_ylabel('A (MBq)')
+    axes[1].set_title('{} Bi-Exponential'.format(organ))
+
+    axes[2].plot(t[skip_points:],residuals,'o')
+    axes[2].set_title('Residuals')
+    axes[2].set_xlabel('t (days)')
+    axes[2].set_ylabel('A (MBq)')
+
+
+    axes[0].set_xlim(left=0)
+    axes[0].set_ylim(bottom=0)
+
+    axes[1].set_xlim(left=0)
+    axes[1].set_ylim(a[1:].min()*0.8,a.max()*1.1)
+
+
+    plt.tight_layout()
+
+    if 'save_path' in kwargs:
+        plt.savefig(f"{kwargs['save_path']}/{organ}_biexpuptake_fit.png",dpi=300)
