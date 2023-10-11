6bfee77fdfbebfabaa569caa52f4af764b68d654
sarakurkowska <sarakurkowska95@gmail.com>
2023-10-11 12:11:38 -0700

    json file added

--- doodle/dosimetry/dosemap_analysis.py
+++ doodle/dosimetry/dosemap_analysis.py
@@ -3,10 +3,14 @@ import gatetools as gt
 import itk
 import numpy as np
 import SimpleITK as sitk
-
-
-
 import argparse
+from pathlib import Path
+import json
+
+this_dir=Path(__file__).resolve().parent.parent
+RADIOBIOLOGY_DATA_FILE = Path(this_dir,"data","radiobiology.json")
+
+
 class Dosemap:
     def __init__(self, df, patient_id, cycle, dosemap, roi_masks_resampled, organlist):
         self.df = df
@@ -15,7 +19,15 @@ class Dosemap:
         self.dosemap = dosemap 
         self.roi_masks_resampled = roi_masks_resampled
         self.organlist = organlist
-        
+        print(RADIOBIOLOGY_DATA_FILE)
+        if RADIOBIOLOGY_DATA_FILE.exists():
+            with open(RADIOBIOLOGY_DATA_FILE) as f:
+                self.radiobiology_dic = json.load(f)
+        else:
+            print("The JSON file does not exist.")
+
+        #with open(RADIOBIOLOGY_DATA_FILE) as f:
+        #    self.radiobiology_dic = json.load(f)
         
     def image_visualisation(self, image):
         fig, axs = plt.subplots(1, 3, figsize=(15, 5))
@@ -61,15 +73,27 @@ class Dosemap:
             
     # equation based on the paper Bodei et al. "Long-term evaluation of renal toxicity after peptide receptor radionuclide therapy with 90Y-DOTATOC 
     # and 177Lu-DOTATATE: the role of associated risk factors"
-    def calculate_bed(abs_dose_per_cycle, n_cycles, alpha_beta, t_repair, t_eff):
+    def calculate_bed(self, abs_dose_per_cycle, n_cycles, alpha_beta, t_repair):
         bed = 0
-        for i in range(1, n_cycles + 1):
-            abs_dose = abs_dose_per_cycle[f'{i}']
-            bed += abs_dose + 1/alpha_beta * t_repair/(t_repair + t_eff) * abs_dose**2
+
+
+
+        for i in self.organlist:
+            t_eff['i'] = self.df.loc[self.df['organ'] == 'i']['lamda_eff_1/s'].values[0]
+            for j in range(1, n_cycles + 1):
+                abs_dose = abs_dose_per_cycle[f'{j}']
+                bed['i'] += abs_dose + 1/alpha_beta * t_repair/(t_repair + t_eff['i']) * abs_dose**2
 
         return bed
 
-            
+
+    def dataf(self):
+        #print(self.df)
+        print(self.df.loc[self.df['organ'] == 'Liver']['lamda_eff_1/s'].values[0])
+        intersection = {key: self.organlist[key] for key in self.organlist if key in self.radiobiology_dic and self.organlist[key] == self.radiobiology_dic[key]}
+
+        print(intersection)
+    
     def save_dataframe(self):
         print(self.df)
         patient_data = self.df[(self.df['patient_id'] == self.patient_id) & (self.df['cycle'] == self.cycle)]
