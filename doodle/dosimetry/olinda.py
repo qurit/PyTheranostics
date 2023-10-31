5dff485b9d2bb6cf79450962e5e83c80415c7db0
sarakurkowska <sarakurkowska95@gmail.com>
2023-10-31 14:30:19 -0700

    added create cas file function; it is not finished

--- doodle/dosimetry/olinda.py
+++ doodle/dosimetry/olinda.py
@@ -4,11 +4,13 @@ import numpy as np
 
 
 class Olinda:
-    def __init__(self, df):
+    def __init__(self, df, isotope):
         self.df = df
+        self.isotope = isotope
 
         
     def phantom_data(self):
+        print(self.df)
         lesion_df = self.df[self.df['organ'].str.contains('Lesion', case=False, na=False)]
         
         ## Prepare df for Olinda 
@@ -28,20 +30,51 @@ class Olinda:
         # renaming
         self.df['organ'] = self.df['organ'].replace('Bladder_Experimental', 'Urinary Bladder Contents')
 
-
+        blankIndex=[''] * len(self.df)
+        self.df.index=blankIndex
         print(self.df)
+
 
         self.organlist = self.df['organ'].unique()
         this_dir=path.dirname(__file__)
         PHANTOM_PATH = path.join(this_dir,'phantomdata')
         self.phantom_mass = pd.read_csv(path.join(PHANTOM_PATH,'human_phantom_masses.csv'))
         print(np.array(self.phantom_mass['Organ']))
-        not_inphantom=[]
+        self.not_inphantom=[]
         for org in self.organlist: 
             if org not in np.array(self.phantom_mass['Organ']):            
-                not_inphantom.append(org)
-        print('These organs from the biodi are not modelled in the phantom\n{}'.format(not_inphantom))
+                self.not_inphantom.append(org)
+        print('These organs are not modelled in the phantom\n{}'.format(self.not_inphantom))
             
-    def template_data(self):
+    def create_case_file(self):
+        this_dir=path.dirname(__file__)
         TEMPLATE_PATH = path.join(this_dir,"olindaTemplates")
+        template=pd.read_csv(path.join(TEMPLATE_PATH,'adult_male.cas'))
+        template.columns=['Data']
+        
+        #modify the isotope in the template
+        ind=template[template['Data']=='[BEGIN NUCLIDES]'].index
+        template.loc[ind[0]+1,'Data']=self.isotope + '|'
+        
+        print(self.df.drop(set(self.not_inphantom).intersection(set(self.df.index))).index)
+        #change the kinetics for each input organ
+        for org in self.df.drop(set(self.not_inphantom).intersection(set(self.df.index))).index:  #ignore the tumor here
+            temporg=org
+            uptakepos=2
+            
+
+                
+            ind=template[template['Data'].str.contains(temporg)].index
+            sourceorgan=template.iloc[ind[0]].str.split('|')[0][0]
+            massorgan=template.iloc[ind[0]].str.split('|')[0][1]
+            kineticdata=self.df.loc[org]['tiac_h']
+            
+            if np.isnan(kineticdata):
+                kineticdata=0
+
+            template.iloc[ind[0]]=sourceorgan+'|'+massorgan+'|'+'{:7f}'.format(kineticdata)
+            template.iloc[ind[uptakepos]]=sourceorgan+'|'+'{:7f}'.format(kineticdata)
+
+        
+        print(template)
         #####
