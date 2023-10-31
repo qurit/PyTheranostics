658a4f55fd6627eec0acbb6f2e66a0777327663d
sarakurkowska <sarakurkowska95@gmail.com>
2023-10-31 14:52:42 -0700

    triexp added

--- doodle/fits/fits.py
+++ doodle/fits/fits.py
@@ -9,14 +9,13 @@ from scipy.optimize import curve_fit
 
 
 def monoexp_fun(t,a,b):
-    return a*exp(-b*t)
+    return a * exp(-b * x)
 
 def biexp_fun(t, a, b,c,d):
-    return a*exp(-b*t) + c*exp(-d*t)
+    return a * exp(-b * x) + c * exp(-d * x)
 
-# def triexp_fun(x,a,b,c,d,e):
-#     decayconst = 0.004344987591895751
-#     return a*exp(-b*x) + c*exp(-d*x) +e*exp(-decayconst*x)
+def triexp_fun(x, a, b, c, d, e, f):
+    return a * exp(-b * x) + c * exp(-d * x) + e * exp(-f * x)
 
 def find_a_initial(f, b, t):
     return f * exp(b * t)
@@ -55,9 +54,8 @@ def fit_monoexp(t,a,decayconst,skip_points=0,ignore_weights=True,monoguess=(1,1)
         popt, pconv = curve_fit(monoexp_fun,t[skip_points:],a[skip_points:],sigma=weights[skip_points:],
                                 p0=monoguess,bounds=([0,upper_bound],np.inf),maxfev=maxev)
     else:
-        print("Im here")
-        popt, pconv = curve_fit(monoexp_fun,t[skip_points:],a[skip_points:],sigma=weights,p0=monoguess,
-                                maxfev=maxev)
+        popt, pconv = curve_fit(monoexp_fun,t[skip_points:],a[skip_points:],sigma=weights,
+                                p0=monoguess,bounds=([0,upper_bound],np.inf), maxfev=maxev)
 
 
     [r_squared, residuals] = get_residuals(t,a,skip_points,popt,eq='monoexp')
@@ -143,3 +141,33 @@ def fit_biexp_uptake(t,a,decayconst,skip_points=0,ignore_weights=True,append_zer
 
     return popt,tt,yy,residuals
 
+def fit_triexp(t,a,decayconst,skip_points=0,ignore_weights=True,append_zero=True,uptakeguess=(1,1,1,1,1,1),maxev=100000):
+
+     # Append point (0,0) if flag set to true.
+    if append_zero:
+        t = np.append(0,t)
+        a = np.append(0,a)
+        if ignore_weights:
+            weights = None
+        else:
+            weights = sigmas
+            weights = np.append(1,weights)
+
+
+    popt, pconv = curve_fit(triexp_fun,t,a,sigma=weights,
+                            p0=uptakeguess,bounds=None,maxfev=maxev)
+    
+
+    [r_squared, residuals] = get_residuals(t,a,skip_points,popt,eq='triexp')
+
+    popt = np.append(popt,r_squared)
+
+    # create times for displaying the function
+    tt = np.linspace(0,t.max()*2.5,1000)
+    yy = biexp_fun(tt,*popt[:6])
+    # yy = monoexp_fun(tt[tt>=t[skip_points]],*popt[:2])
+
+    # popt_df = pd.DataFrame(popt).T
+    # popt_df.columns = ['A0','lambda_eff','R2']
+
+    return popt,tt,yy,residuals
