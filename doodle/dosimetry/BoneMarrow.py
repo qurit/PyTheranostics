611a46a07f5a108090368eaeda33ee0a315f62dd
Pedro L. Esquinas <pedro.esquinasfernandez@bccancer.bc.ca>
2023-12-27 10:37:57 -0800

    Add mono-exponential.

--- doodle/dosimetry/BoneMarrow.py
+++ doodle/dosimetry/BoneMarrow.py
@@ -1,8 +1,8 @@
 import pandas
 from typing import Tuple
+from doodle.fits import fit_monoexp
 
-
-def blood_based_selfdose(df: pandas.DataFrame) -> Tuple[float, float]:
+def blood_based_selfdose(df: pandas.DataFrame, decay_constant: float) -> Tuple[float, float]:
 
     """
     Computes the bone-marrow self-dose due to activity concentration in the bone-marrow, following
@@ -10,6 +10,11 @@ def blood_based_selfdose(df: pandas.DataFrame) -> Tuple[float, float]:
 
     Returns:
     Dose_BM <- Extra Cellular Fluid, Dose_BM <- Blood Cells
+
+    Limitations:
+     - Assumes mono-exponential model of Time Activity Concentration vs Time.
+     - The activity within the bone-marrow cells must be calculated from quantitative imaging
+       in different regions of the bone barrow.
 
     """
 
@@ -19,17 +24,21 @@ def blood_based_selfdose(df: pandas.DataFrame) -> Tuple[float, float]:
 
     # Parameters:
     RMECFF = 0.19
-    HTC = 1
+    HCT = 1  # Can be used to convert from Ablood to Aplasma.
     RMBLR = 0.36  # From Sgouros; J Nucl Med 1993;34(4):689–94 ?
                   # A RMBLR = 1 also found for Lu-177 peptides?
 
 
-    # TODO: Compute Time-Integrated Activity Concetrations
-    TIA_plasma = 0
-    TIA_blood = 0
+    # Compute Time-Integrated Activity Concetrations. 
+    TAC_blood_param, _ = fit_monoexp(time=df["Time"].to_numpy(),
+                             activity=df["Activity_Conc"].to_numpy(),
+                             decayconst=decay_constant,
+                             monoguess=(df["Time"].iloc[0], decay_constant))
+    TIAC_blood = TAC_blood_param[0] / TAC_blood_param[1]  # Use analytical formula for mono-exponential decay.
+    
 
-    # Compute Dose 
-    dose_ECF = TIA_plasma * RMECFF * m_bm * s_bm_bm
-    dose_Blood = TIA_blood * RMBLR * m_bm * s_bm_bm
+    # Compute Dose: Assume TIAC_blood = TIAC_plasma
+    dose_ECF = TIAC_blood * RMECFF * m_bm * s_bm_bm  # Should use Plasma TIAC.
+    dose_Blood = TIAC_blood * RMBLR * m_bm * s_bm_bm
 
     return dose_ECF, dose_Blood
