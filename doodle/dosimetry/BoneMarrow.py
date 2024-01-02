485050cd56a8be63d07ea04f1368fe60d3fc54f4
Pedro L. Esquinas <pedro.esquinasfernandez@bccancer.bc.ca>
2024-01-02 09:47:54 -0800

    Simplify red_marrow_selfdose according to EANM recommendations

--- doodle/dosimetry/BoneMarrow.py
+++ doodle/dosimetry/BoneMarrow.py
@@ -1,47 +1,53 @@
-import pandas
 from typing import Tuple
-from doodle.fits import fit_monoexp
-from dosimetry.olinda import load_s_values, load_phantom_mass
+import json
+import math
 
-def blood_based_selfdose(df: pandas.DataFrame, radionuclide: str, gender: str) -> Tuple[float, float]:
+import pandas
+from dosimetry.olinda import load_phantom_mass, load_s_values
+
+from doodle.fits import fit_monoexp
+
+
+def marrow_selfdose(df_blood: pandas.DataFrame, radionuclide: str, gender: str) -> float:
 
     """
-    Computes the bone-marrow self-dose due to activity concentration in the bone-marrow, following
-    EANM Guidelines: https://www.eanm.org/publications/guidelines/EJNMMI_Bone_Marrow_Dosimetry_06_2010.pdf
+    Computes the bone-marrow self-dose due to activity concentration in the red-marrow, following
+    EANM Recommendations. This is the blood-based method, Equation (13) in:
+
+    https://link.springer.com/article/10.1007/s00259-022-05727-7
 
     Returns:
-    Dose_BM <- Extra Cellular Fluid, Dose_BM <- Blood Cells
+    Dose (RM <-- RM)
 
     Limitations:
      - Assumes mono-exponential model of Time Activity Concentration vs Time.
-     - The activity within the bone-marrow cells must be calculated from quantitative imaging
-       in different regions of the bone barrow.
-
     """
+    # Load Radionuclide data
+    with open("../data/isotopes.json", "r") as rad_data:
+        radnuclide_data = json.load(rad_data)
+    
+    if radionuclide not in radnuclide_data:
+        raise ValueError(f"Data for {radionuclide} is not available.")
+    # TODO: half_life should be in seconds in the database.
+    decay_constant = math.log(2) / (radnuclide_data[radionuclide]["half_life"] * 24 * 3600)
 
-    # S-Values
+    # Load S-Values
     s_values = load_s_values(gender=gender, radionuclide=radionuclide)
-    s_bm_bm =  s_values["Red Marrow"]["Red Mar."] # Red-Marrow -> Red-Marrow S-Factor, Adult Male in mSv/MBq s.
+    s_bm_bm =  s_values["Red Marrow"]["Red Mar."] # Red-Marrow -> Red-Marrow S-Factor, Adult in mSv/MBq s.
     m_bm = load_phantom_mass(gender=gender, organ="Red  Marrow")  # in Grams.
 
     # Parameters:
-    RMECFF = 0.19
-    HCT = 1  # Can be used to convert from Ablood to Aplasma.
-    RMBLR = 0.36  # From Sgouros; J Nucl Med 1993;34(4):689–94 ?
-                  # A RMBLR = 1 also found for Lu-177 peptides?
-
+    RMBLR = 1.0  # Activity concentration in Red Marrow over blood. Generally 1 for PSMA and SSRT.
 
     # Compute Time-Integrated Activity Concetrations. 
-    TAC_blood_param, _ = fit_monoexp(time=df["Time"].to_numpy(),
-                             activity=df["Activity_Conc"].to_numpy(),
+    TAC_blood_param, _ = fit_monoexp(time=df_blood["Time"].to_numpy(),
+                             activity=df_blood["Activity_Conc"].to_numpy(),
                              decayconst=decay_constant,
-                             monoguess=(df["Time"].iloc[0], decay_constant))
+                             monoguess=(df_blood["Time"].iloc[0], decay_constant))
     TIAC_blood = TAC_blood_param[0] / TAC_blood_param[1]  # Use analytical formula for mono-exponential decay.
-    
 
-    # Compute Dose: Assume TIAC_blood = TIAC_plasma
-    # Activity Concentration should be in MBq / grams.
-    dose_ECF = TIAC_blood * RMECFF * m_bm * s_bm_bm  # Should use Plasma TIAC.
-    dose_Blood = TIAC_blood * RMBLR * m_bm * s_bm_bm
 
-    return dose_ECF, dose_Blood
+    # Compute Dose: Activity Concentration should be in MBq / grams.
+    dose_rm_rm = TIAC_blood * RMBLR * m_bm * s_bm_bm
+
+    return dose_rm_rm
