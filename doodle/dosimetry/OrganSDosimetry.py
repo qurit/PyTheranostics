f484d76f7bc9aac1d07cf9b51a1a11fb356ce8ee
sarakurkowska <sarakurkowska95@gmail.com>
2024-02-28 15:59:36 -0800

    finish base  organ dosimetry class

--- doodle/dosimetry/OrganSDosimetry.py
+++ doodle/dosimetry/OrganSDosimetry.py
@@ -1,28 +1,53 @@
-from doodle.dosimetry import BaseDosimetry
+from doodle.dosimetry.BaseDosimetry import BaseDosimetry
+from typing import Any, Dict, List, Optional
+from doodle.ImagingDS.LongStudy import LongitudinalStudy
+
+import datetime
+import numpy
+import pandas
+from os import path
 
 class OrganSDosimetry(BaseDosimetry):
-    def prepare_data(self):
-        lesion_df = self.tiac_for_masks[self.tiac_for_masks.index.str.contains('Lesion', case=False, na=False)]
+    """Organ S Value Dosimetry Class. Takes Nuclear Medicine Data to perform Organ-level 
+    patient-specific dosimetry by computing organ time-integrated activity curves and
+    leveraging organ level S-values. It supports external software: Olinda/EXM. """
+    def __init__(self,
+                 config: Dict[str, Any],
+                 nm_data: LongitudinalStudy, 
+                 ct_data: Optional[LongitudinalStudy],
+                 clinical_data: Optional[pandas.DataFrame] = None
+                 ) -> None:
+        super().__init__(config, nm_data, ct_data, clinical_data)
         
-        self.tiac_for_masks['Volume_CT_mL'] = self.tiac_for_masks['Volume_CT_mL'].apply(lambda x: numpy.mean(x))
-        phrases_to_exclude = ['Cavity', 'Femur', 'Humerus', 'Reference', 'TotalTumorBurden', 'Kidney_L_a', 'Kidney_R_a', 'Lesion', 'LN_Iliac']
-        phrases_to_exclude.extend([f'L{i}' for i in range(10)])  
-        self.tiac_for_masks = self.tiac_for_masks[~self.tiac_for_masks.index.str.contains('|'.join(phrases_to_exclude), case=False, na=False)]
-        self.tiac_for_masks = self.tiac_for_masks[['Volume_CT_mL', 'TIAC_h']]
+        """Inputs:
+            config: Configuration parameters for dosimetry calculations, a Dict.
+                    Note: defined VOIs should have the same naming convention as source organs in Olinda.
+                    We included method prepare_df() that combines kidneys and salivary glands into one VOI.
+            nm_data: longitudinal, quantitative, nuclear-medicine imaging data, type LongitudinalStudy.
+                     Note: voxel values should be in units of Bq/mL.
+            ct_data: longitudinal CT imaging data, type LongitudinalStudy,
+                     Note: voxel values should be in HU units.
+            clinical_data: clinical data such as blood sampling, an optional pandas DataFrame. 
+                     Note: blood counting should be in units of Bq/mL.
+        """    
+
+    def prepare_data(self) -> None:
+        self.results_olinda = self.results[['Volume_CT_mL', 'TIAC_h']].copy()
+        self.results_olinda['Volume_CT_mL'] = self.results_olinda['Volume_CT_mL'].apply(lambda x: numpy.mean(x))
+        self.results_olinda = self.results_olinda[['Volume_CT_mL', 'TIAC_h']]
         
-        columns_to_sum = ['TIAC_h', 'Volume_CT_mL']
-        self.tiac_for_masks.loc['Kidneys'] = self.tiac_for_masks.loc[['Kidney_R_m', 'Kidney_L_m']].sum()
-        self.tiac_for_masks.loc['Kidneys', columns_to_sum] = self.tiac_for_masks.loc[['Kidney_R_m', 'Kidney_L_m'], columns_to_sum].sum()
-        self.tiac_for_masks = self.tiac_for_masks.drop(['Kidney_R_m', 'Kidney_L_m'])
-        self.tiac_for_masks.loc['Salivary Glands'] = self.tiac_for_masks.loc[['ParotidglandL', 'ParotidglandR', 'SubmandibularglandL', 'SubmandibularglandR']].sum()
-        self.tiac_for_masks.loc['Salivary Glands', columns_to_sum] = self.tiac_for_masks.loc[['ParotidglandL', 'ParotidglandR', 'SubmandibularglandL', 'SubmandibularglandR'], columns_to_sum].sum()
-        self.tiac_for_masks = self.tiac_for_masks.drop(['ParotidglandL', 'ParotidglandR', 'SubmandibularglandL', 'SubmandibularglandR'])
+        self.results_olinda.loc['Kidneys'] = self.results_olinda.loc[['Kidney_R_m', 'Kidney_L_m']].sum()
+        self.results_olinda = self.results_olinda.drop(['Kidney_R_m', 'Kidney_L_m'])
+        
+        self.results_olinda.loc['Salivary Glands'] = self.results_olinda.loc[['ParotidglandL', 'ParotidglandR', 'SubmandibularglandL', 'SubmandibularglandR']].sum()
+        self.results_olinda = self.results_olinda.drop(['ParotidglandL', 'ParotidglandR', 'SubmandibularglandL', 'SubmandibularglandR'])
 
-        organs = self.tiac_for_masks.index[self.tiac_for_masks.index != 'WBCT']
-        self.tiac_for_masks.loc['WBCT', columns_to_sum] = self.tiac_for_masks.loc['WBCT', columns_to_sum] - self.tiac_for_masks.loc[organs, columns_to_sum].sum()
+        organs = self.results_olinda.index[self.results_olinda.index != 'WBCT']
+        self.results_olinda.loc['WBCT'] = self.results_olinda.loc['WBCT'] - numpy.sum(self.results_olinda.loc[organs])
 
-        self.tiac_for_masks = self.tiac_for_masks.rename(index={'Bladder_Experimental': 'Urinary Bladder Contents', 'Skeleton': 'Cortical Bone', 'WBCT': 'Total Body'}) # TODO Cortical Bone vs Trabercular Bone
-
+        self.results_olinda = self.results_olinda.rename(index={'Bladder_Experimental': 'Urinary Bladder Contents', 
+                                                  'Skeleton': 'Cortical Bone', 
+                                                  'WBCT': 'Total Body'}) # TODO Cortical Bone vs Trabercular Bone
             
     def create_Olinda_file(self, dirname, savefile=False):
         this_dir=path.dirname(__file__)
@@ -36,13 +61,13 @@ class OrganSDosimetry(BaseDosimetry):
         ind=template[template['Data']=='[BEGIN NUCLIDES]'].index
         template.loc[ind[0]+1,'Data']= isotope + '|'
         
-        for org in self.tiac_for_masks.index:  
+        for org in self.results_olinda.index:  
             temporg = org
             ind=template[template['Data'].str.contains(temporg)].index
             sourceorgan=template.iloc[ind[0]].str.split('|')[0][0]
             massorgan=template.iloc[ind[0]].str.split('|')[0][1]
-            kineticdata=self.tiac_for_masks.loc[org]['TIAC_h']
-            massdata=round(self.tiac_for_masks.loc[org]['Volume_CT_mL'], 1)
+            kineticdata=self.results_olinda.loc[org]['TIAC_h']
+            massdata=round(self.results_olinda.loc[org]['Volume_CT_mL'], 1)
             template.iloc[ind[0]]=sourceorgan+'|'+str(massorgan)+'|'+'{:7f}'.format(kineticdata)         
             if len(ind) == 2:
                 template.iloc[ind[1]]=sourceorgan+'|'+'{:7f}'.format(kineticdata)            
@@ -63,5 +88,38 @@ class OrganSDosimetry(BaseDosimetry):
 
             template.to_csv(str(dirname) + '/' + f"{self.config['PatientID']}.cas", index=False)
 
-    def read_Olinda_results(self) -> None:
-        pass 
+    def read_Olinda_results(self, 
+                            olinda_results_path: str
+                            ) -> None:
+        data = []
+        with open(olinda_results_path, 'r') as file:
+        
+            extract_lines = False
+            for line in file:
+                stripped_line = line.strip()
+                if stripped_line.startswith("Target Organ,Alpha,Beta,Gamma,Total,ICRP-103 ED"):
+                    extract_lines = True
+                elif stripped_line.startswith("Target Organ Name,Mass [g],"):
+                    extract_lines = False
+                if extract_lines:
+                    if stripped_line.startswith('Effective Dose'):
+                        stripped_line = stripped_line.replace('Effective Dose', 'Effective Dose,,,,')
+                    stripped_line = stripped_line.rstrip(',')
+                    data.append(stripped_line.split(','))
+        
+        df_ad = pandas.DataFrame(data[1:], columns=data[0])
+        df_ad = df_ad.set_index('Target Organ')
+        df_ad = df_ad.dropna(axis=0)        
+        df_ad['Total'] = pandas.to_numeric(df_ad['Total'], errors='coerce')
+        df_ad['Total'].fillna(0, inplace=True)
+        df_ad['AD[Gy]'] = df_ad['Total'] * float(self.config['InjectedActivity']) / 1000
+        self.df_ad = df_ad
+        
+
+    
+    def compute_dose(self):
+        self.compute_tiac()
+        self.prepare_data()
+        
+        
+        
