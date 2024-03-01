7d9d6fd5cc471b63aacf6f49818e8bd9dac4f924
sarakurkowska <sarakurkowska95@gmail.com>
2024-03-01 11:06:37 -0800

    change column name in bed method

--- doodle/dosimetry/BaseDosimetry.py
+++ doodle/dosimetry/BaseDosimetry.py
@@ -278,7 +278,7 @@ class BaseDosimetry(metaclass=abc.ABCMeta):
         for organ in organs:
             t_repair = self.radiobiology_dic[organ]['t_repair']
             alpha_beta = self.radiobiology_dic[organ]['alpha_beta']
-            AD = float(bed_df.loc[bed_df.index == organ]['Total'].values[0])  * float(self.config['InjectedActivity']) / 1000 # Gy
+            AD = float(bed_df.loc[bed_df.index == organ]['AD[Gy/GBq]'].values[0])  * float(self.config['InjectedActivity']) / 1000 # Gy
             if kinetic == 'monoexp':
                 t_eff = numpy.log(2) / self.results.loc[organ]['Fit_params'][1]
                 bed[organ] = AD + 1/alpha_beta * t_repair/(t_repair + t_eff) * AD**2
