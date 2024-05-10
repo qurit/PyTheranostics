1c51a679be233f64a90947a50ff296385c52cebc
Pedro L. Esquinas <pedro.esquinasfernandez@bccancer.bc.ca>
2024-05-10 14:21:17 -0700

    get OrganSDosimetry changes from dev

--- doodle/dosimetry/OrganSDosimetry.py
+++ doodle/dosimetry/OrganSDosimetry.py
@@ -5,19 +5,21 @@ from doodle.ImagingDS.LongStudy import LongitudinalStudy
 import datetime
 import numpy
 import pandas
+import re
 from os import path
 
 class OrganSDosimetry(BaseDosimetry):
     """Organ S Value Dosimetry Class. Takes Nuclear Medicine Data to perform Organ-level 
     patient-specific dosimetry by computing organ time-integrated activity curves and
-    leveraging organ level S-values. It supports external software: Olinda/EXM. """
+    leveraging organ level S-values. Currently, it supports external software: Olinda/EXM. """ # TODO: add support for MirdCalc and IDAC
     def __init__(self,
                  config: Dict[str, Any],
                  nm_data: LongitudinalStudy, 
-                 ct_data: LongitudinalStudy,
+                 ct_data: Optional[LongitudinalStudy],
                  clinical_data: Optional[pandas.DataFrame] = None
                  ) -> None:
         super().__init__(config, nm_data, ct_data, clinical_data)
+        self.check_mandatory_fields_organ()
         
         """Inputs:
             config: Configuration parameters for dosimetry calculations, a Dict.
@@ -30,16 +32,24 @@ class OrganSDosimetry(BaseDosimetry):
             clinical_data: clinical data such as blood sampling, an optional pandas DataFrame. 
                      Note: blood counting should be in units of Bq/mL.
         """    
-
-    def prepare_data(self) -> None:
-        self.results_olinda = self.results[['Volume_CT_mL', 'TIA_h']].copy()
-        self.results_olinda['Volume_CT_mL'] = self.results_olinda['Volume_CT_mL'].apply(lambda x: numpy.mean(x))
-        self.results_olinda = self.results_olinda[['Volume_CT_mL', 'TIA_h']]
         
-        self.results_olinda.loc['Kidneys'] = self.results_olinda.loc[['Kidney_R_m', 'Kidney_L_m']].sum()
+    def check_mandatory_fields_organ(self) -> None:
+
+        if "Organ" not in self.config["Level"]:
+            print("Verify the level on which dosimetry should be performed.")
+        
+        return None
+        
+    def prepare_data(self) -> None:
+        """Creates .cas file that can be exported to Olinda/EXM."""
+        self.results_olinda = self.results[['Volume_CT_mL', 'TIAC_h']].copy()
+        self.results_olinda['Volume_CT_mL'] = self.results_olinda['Volume_CT_mL'].apply(lambda x: numpy.mean(x))
+        self.results_olinda = self.results_olinda[['Volume_CT_mL', 'TIAC_h']]
+        
+        self.results_olinda.loc['Kidneys'] = self.results_olinda.loc[['Kidney_R_m', 'Kidney_L_m']].sum() # TODO: change vois name for more generic
         self.results_olinda = self.results_olinda.drop(['Kidney_R_m', 'Kidney_L_m'])
         
-        self.results_olinda.loc['Salivary Glands'] = self.results_olinda.loc[['ParotidglandL', 'ParotidglandR', 'SubmandibularglandL', 'SubmandibularglandR']].sum()
+        self.results_olinda.loc['Salivary Glands'] = self.results_olinda.loc[['ParotidglandL', 'ParotidglandR', 'SubmandibularglandL', 'SubmandibularglandR']].sum() # TODO: change vois name for more generic
         self.results_olinda = self.results_olinda.drop(['ParotidglandL', 'ParotidglandR', 'SubmandibularglandL', 'SubmandibularglandR'])
 
         organs = self.results_olinda.index[self.results_olinda.index != 'WBCT']
@@ -51,41 +61,61 @@ class OrganSDosimetry(BaseDosimetry):
                                                   'BoneMarrow': 'Red Marrow'}) # TODO Cortical Bone vs Trabercular Bone
         
         self.results_olinda.loc['Red Marrow']['Volume_CT_mL'] = 1170 # TODO volume hardcoded, think about alternatives
-            
-    def create_Olinda_file(self, dirname, savefile=False):
+    
+    def create_output_file(self, 
+                           dirname: str, 
+                           savefile: bool = False
+                           ) -> None:  
+        if self.config["OutputFormat"] == "Olinda":  
+            self.create_Olinda_file(dirname, savefile)
+        else:
+            print("In the current version, we only support Olinda as external organ S-value software.") # TODO: other software case files
+                
+    def create_Olinda_file(self, 
+                           dirname: str, 
+                           savefile: bool = False
+                           ) -> None:
+        """Creates .cas file that can be exported to Olinda/EXM."""
         this_dir=path.dirname(__file__)
         TEMPLATE_PATH = path.join(this_dir,"olindaTemplates")
-        template=pandas.read_csv(path.join(TEMPLATE_PATH,'adult_male.cas'))
-        template.columns=['Data']
-        if self.config["Radionuclide"] == 'Lu177':
-            isotope = 'Lu-177'
-        else:
-            print('Add isotope')
-        ind=template[template['Data']=='[BEGIN NUCLIDES]'].index
-        template.loc[ind[0]+1,'Data']= isotope + '|'
         
-        for org in self.results_olinda.index:  
-            temporg = org
-            ind=template[template['Data'].str.contains(temporg)].index
-            sourceorgan=template.iloc[ind[0]].str.split('|')[0][0]
-            print(sourceorgan)
-            massorgan=template.iloc[ind[0]].str.split('|')[0][1]
-            kineticdata=self.results_olinda.loc[org]['TIA_h']
-            massdata=round(self.results_olinda.loc[org]['Volume_CT_mL'], 1)
-            template.iloc[ind[0]]=sourceorgan+'|'+str(massorgan)+'|'+'{:7f}'.format(kineticdata)  
-            print(len(ind))       
-            if len(ind) == 2:
-                template.iloc[ind[1]]=sourceorgan+'|'+'{:7f}'.format(kineticdata)            
-            elif len(ind) == 3:
-                template.iloc[ind[1]]=sourceorgan+'|'+str(massdata)
-                template.iloc[ind[2]]=sourceorgan+'|'+'{:7f}'.format(kineticdata)
+        if self.config["Gender"] == "Male":
+            template=pandas.read_csv(path.join(TEMPLATE_PATH,'adult_male.cas'))
+        elif self.config["Gender"] == "Female":
+            template=pandas.read_csv(path.join(TEMPLATE_PATH,'adult_female.cas'))
+        else:
+            print('Ensure that you correctly wrote patient gender in config file. We support: Male and Female.')
+        
+        template.columns=['Data']
+        match = re.match(r"([a-zA-Z]+)([0-9]+)", self.config["Radionuclide"])
+        letters, numbers = match.groups()
+        formatted_radionuclide = f"{letters}-{numbers}"
+        
+        ind=template[template['Data']=='[BEGIN NUCLIDES]'].index
+        template.loc[ind[0]+1,'Data']= formatted_radionuclide + '|'
+             
+        for organ in self.results_olinda.index:
+            indices = template[template['Data'].str.contains(organ)].index
+
+            source_organ = template.iloc[indices[0]].str.split('|')[0][0]
+            mass_phantom = template.iloc[indices[0]].str.split('|')[0][1]
+            kinetic_data = self.results_olinda.loc[organ]['TIAC_h']
+            mass_data = round(self.results_olinda.loc[organ]['Volume_CT_mL'], 1)
+
+            # Update the template DataFrame
+            template.iloc[indices[0]] = f"{source_organ}|{mass_phantom}|{'{:7f}'.format(kinetic_data)}"
+
+            if len(indices) == 2:
+                template.iloc[indices[1]] = f"{source_organ}|{'{:7f}'.format(kinetic_data)}"
+            elif len(indices) == 3:
+                template.iloc[indices[1]] = f"{source_organ}|{mass_data}"
+                template.iloc[indices[2]] = f"{source_organ}|{'{:7f}'.format(kinetic_data)}"
             else:
-                print('Double check where the organ appears in the template')
+                print('Double-check where the organ appears in the template.')
 
         template = template.replace('TARGET_ORGAN_MASSES_ARE_FROM_USER_INPUT|FALSE', 'TARGET_ORGAN_MASSES_ARE_FROM_USER_INPUT|TRUE')
 
-        now = datetime.datetime.now()
-        template.columns=['Saved on ' + now.strftime("%m.%d.%Y") +' at ' + now.strftime('%H:%M:%S')]
+        template.columns=['Saved on ' + datetime.datetime.now().strftime("%m.%d.%Y") +' at ' + datetime.datetime.now().strftime('%H:%M:%S')]
 
         if savefile==True:
             if not path.exists(dirname):
@@ -93,12 +123,23 @@ class OrganSDosimetry(BaseDosimetry):
 
             template.to_csv(str(dirname) + '/' + f"{self.config['PatientID']}.cas", index=False)
 
+    def read_results(self, 
+                     olinda_results_path: str
+                     ) -> None:  
+        if self.config["OutputFormat"] == "Olinda":  
+            self.read_Olinda_results(olinda_results_path)
+            
     def read_Olinda_results(self, 
                             olinda_results_path: str
                             ) -> None:
+        """Reads .txt results file from Olinda.""" 
+        if not olinda_results_path.endswith(".txt"):
+            print('Please export result from Olinda in .txt file.') ## TODO: Add .csv extension results
+            return
+    
         data = []
         with open(olinda_results_path, 'r') as file:
-        
+
             extract_lines = False
             for line in file:
                 stripped_line = line.strip()
@@ -112,21 +153,24 @@ class OrganSDosimetry(BaseDosimetry):
                     stripped_line = stripped_line.rstrip(',')
                     data.append(stripped_line.split(','))
         
+        if not data:
+            print('No relevant data found in the file.')
+            return
+    
         df_ad = pandas.DataFrame(data[1:], columns=data[0])
         df_ad = df_ad.set_index('Target Organ')
         df_ad = df_ad.dropna(axis=0)        
         df_ad['Total'] = pandas.to_numeric(df_ad['Total'], errors='coerce')
         df_ad['Total'].fillna(0, inplace=True)
         df_ad['AD[Gy]'] = df_ad['Total'] * float(self.config['InjectedActivity']) / 1000
+        df_ad = df_ad.rename(columns={"Total":"AD[Gy/GBq]"})
         self.df_ad = df_ad
         
-        return None
-        
-    def compute_dose(self) -> None:
-        self.compute_tia()
+
+    def compute_dose(self):
+        #self.exlude_lesions_from_healthy_organs() # TODO: add that method in BaseDosimetry
+        self.compute_tiac()
         self.prepare_data()
-        # TODO: finish-up the pipeline up to the Olinda File Export.
-        return None
         
         
         
