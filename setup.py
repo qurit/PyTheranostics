863f6bbf380c93e2a0759586c8329d53ad402883
Carlos Uribe <curibe@bccrc.ca>
2025-05-19 12:53:24 -0700

    Add test dependencies

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='pytheranostics',
-      version='0.1',
-      description='A library of tools to process nuclear medicine scans and take them through the dosimetry workflow to calculate the absorbed dose in target organs. ',
+      version='0.1.0',
+      description='A library of tools to process nuclear medicine scans and take them through the dosimetry workflow to calculate the absorbed dose in target organs.',
       url='https://github.com/qurit/PyTheranostics',
       author='Carlos Uribe, PhD, MCCPM',
       author_email='curibe@bccrc.ca',
-      license='LICENSE',
+      license='MIT',
       packages=[
         'pytheranostics',
         'pytheranostics.calibrations',
@@ -17,9 +17,30 @@ setup(name='pytheranostics',
         'pytheranostics.qc',
         'pytheranostics.segmentation',
         'pytheranostics.registration',
-        'pytheranostics.shared'],
-      include_package_data=True,
-      install_requires = [
-          'numpy','matplotlib','pandas','pydicom','openpyxl','rt-utils','scikit-image', 'simpleitk'
+        'pytheranostics.shared'
       ],
+      include_package_data=True,
+      install_requires=[
+          'numpy',
+          'matplotlib',
+          'pandas',
+          'pydicom',
+          'openpyxl',
+          'rt-utils',
+          'scikit-image',
+          'simpleitk',
+          'lmfit'
+      ],
+      extras_require={
+          'dev': [
+              'pytest>=7.0',
+              'pytest-cov>=4.0',
+              'flake8>=6.0',
+              'black>=23.0',
+              'mypy>=1.0',
+              'sphinx>=7.0',
+              'sphinx-rtd-theme>=1.0'
+          ]
+      },
+      python_requires='>=3.8',
       zip_safe=False)
