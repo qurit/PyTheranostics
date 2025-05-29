172991c259b76081efa73264457deab2314c75a4
Carlos Uribe <curibe@bccrc.ca>
2025-05-28 22:23:15 -0700

    Change to lazy import to make it faster and only import modules as they are needed

--- pytheranostics/__init__.py
+++ pytheranostics/__init__.py
@@ -12,50 +12,6 @@ __author__ = 'Carlos Uribe, PhD, MCCPM'
 __email__ = 'curibe@bccrc.ca'
 __license__ = 'MIT'
 
-# Core functionality imports
-from pytheranostics.qc.planar_qc import PlanarQC
-from pytheranostics.qc.dosecal_qc import DosecalQC
-from pytheranostics.qc.spect_qc import SPECTQC
-
-# Shared utilities
-from pytheranostics.shared.radioactive_decay import (
-    decay_act,
-    get_activity_at_injection
-)
-from pytheranostics.shared.evaluation_metrics import perc_diff
-from pytheranostics.shared.corrections import tew_scatt
-
-# Calibration tools
-from pytheranostics.calibrations.gamma_camera import GammaCamera
-
-# Visualization
-from pytheranostics.plots.plots import (
-    ewin_montage,
-    plot_tac_residuals
-)
-
-# Image processing
-from pytheranostics.segmentation.tools import rtst_to_mask
-
-# Analysis tools
-from pytheranostics.fits.fits import (
-    monoexp_fun,
-    biexp_fun,
-    triexp_fun
-)
-
-# DICOM handling
-from pytheranostics.dicomtools.dicomtools import DicomModify
-
-# Dosimetry imports
-from pytheranostics.dosimetry.OrganSDosimetry import OrganSDosimetry
-from pytheranostics.dosimetry.VoxelSDosimetry import VoxelSDosimetry
-from pytheranostics.dosimetry.olinda import load_phantom_mass
-from pytheranostics.ImagingTools.Tools import load_and_resample_RT
-from pytheranostics.ImagingDS.LongStudy import create_logitudinal_from_dicom
-
-
-
 # Define what should be imported with "from pytheranostics import *"
 __all__ = [
     'PlanarQC',
@@ -79,3 +35,55 @@ __all__ = [
     'create_logitudinal_from_dicom',
     'load_phantom_mass'
 ]
+
+# Lazy imports
+def __getattr__(name):
+    if name == 'PlanarQC':
+        from pytheranostics.qc.planar_qc import PlanarQC
+        return PlanarQC
+    elif name == 'DosecalQC':
+        from pytheranostics.qc.dosecal_qc import DosecalQC
+        return DosecalQC
+    elif name == 'SPECTQC':
+        from pytheranostics.qc.spect_qc import SPECTQC
+        return SPECTQC
+    elif name in ['decay_act', 'get_activity_at_injection']:
+        from pytheranostics.shared.radioactive_decay import decay_act, get_activity_at_injection
+        return globals()[name]
+    elif name == 'perc_diff':
+        from pytheranostics.shared.evaluation_metrics import perc_diff
+        return perc_diff
+    elif name == 'tew_scatt':
+        from pytheranostics.shared.corrections import tew_scatt
+        return tew_scatt
+    elif name == 'GammaCamera':
+        from pytheranostics.calibrations.gamma_camera import GammaCamera
+        return GammaCamera
+    elif name in ['ewin_montage', 'plot_tac_residuals']:
+        from pytheranostics.plots.plots import ewin_montage, plot_tac_residuals
+        return globals()[name]
+    elif name == 'rtst_to_mask':
+        from pytheranostics.segmentation.tools import rtst_to_mask
+        return rtst_to_mask
+    elif name in ['monoexp_fun', 'biexp_fun', 'triexp_fun']:
+        from pytheranostics.fits.fits import monoexp_fun, biexp_fun, triexp_fun
+        return globals()[name]
+    elif name == 'DicomModify':
+        from pytheranostics.dicomtools.dicomtools import DicomModify
+        return DicomModify
+    elif name == 'OrganSDosimetry':
+        from pytheranostics.dosimetry.OrganSDosimetry import OrganSDosimetry
+        return OrganSDosimetry
+    elif name == 'VoxelSDosimetry':
+        from pytheranostics.dosimetry.VoxelSDosimetry import VoxelSDosimetry
+        return VoxelSDosimetry
+    elif name == 'load_phantom_mass':
+        from pytheranostics.dosimetry.olinda import load_phantom_mass
+        return load_phantom_mass
+    elif name == 'load_and_resample_RT':
+        from pytheranostics.ImagingTools.Tools import load_and_resample_RT
+        return load_and_resample_RT
+    elif name == 'create_logitudinal_from_dicom':
+        from pytheranostics.ImagingDS.LongStudy import create_logitudinal_from_dicom
+        return create_logitudinal_from_dicom
+    raise AttributeError(f"module 'pytheranostics' has no attribute '{name}'")
