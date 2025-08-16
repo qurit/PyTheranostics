00ab93c2645455fbf01fc4139a1beaf66c239347
Jason Spence <jasonspencedev@gmail.com>
2025-08-15 20:14:04 -0700

    style: reformat with black - pytheranostics

--- pytheranostics/__init__.py
+++ pytheranostics/__init__.py
@@ -2,10 +2,10 @@
 PyTheranostics - A Python library for nuclear medicine processing and dosimetry
 """
 
-__version__ = '0.1.0'
-__author__ = 'Carlos Uribe, PhD, MCCPM'
-__email__ = 'curibe@bccrc.ca'
-__license__ = 'MIT'
+__version__ = "0.1.0"
+__author__ = "Carlos Uribe, PhD, MCCPM"
+__email__ = "curibe@bccrc.ca"
+__license__ = "MIT"
 
 # Core functionality imports
 from pytheranostics.qc.planar_qc import PlanarQC
@@ -13,10 +13,7 @@ from pytheranostics.qc.dosecal_qc import DosecalQC
 from pytheranostics.qc.spect_qc import SPECTQC
 
 # Shared utilities
-from pytheranostics.shared.radioactive_decay import (
-    decay_act,
-    get_activity_at_injection
-)
+from pytheranostics.shared.radioactive_decay import decay_act, get_activity_at_injection
 from pytheranostics.shared.evaluation_metrics import perc_diff
 from pytheranostics.shared.corrections import tew_scatt
 
@@ -24,39 +21,32 @@ from pytheranostics.shared.corrections import tew_scatt
 from pytheranostics.calibrations.gamma_camera import GammaCamera
 
 # Visualization
-from pytheranostics.plots.plots import (
-    ewin_montage,
-    plot_tac_residuals
-)
+from pytheranostics.plots.plots import ewin_montage, plot_tac_residuals
 
 # Image processing
 from pytheranostics.segmentation.tools import rtst_to_mask
 
 # Analysis tools
-from pytheranostics.fits.fits import (
-    monoexp_fun,
-    biexp_fun,
-    triexp_fun
-)
+from pytheranostics.fits.fits import monoexp_fun, biexp_fun, triexp_fun
 
 # DICOM handling
 from pytheranostics.dicomtools.dicomtools import DicomModify
 
 # Define what should be imported with "from pytheranostics import *"
 __all__ = [
-    'PlanarQC',
-    'DosecalQC',
-    'SPECTQC',
-    'decay_act',
-    'get_activity_at_injection',
-    'perc_diff',
-    'tew_scatt',
-    'GammaCamera',
-    'ewin_montage',
-    'plot_tac_residuals',
-    'rtst_to_mask',
-    'monoexp_fun',
-    'biexp_fun',
-    'triexp_fun',
-    'DicomModify'
+    "PlanarQC",
+    "DosecalQC",
+    "SPECTQC",
+    "decay_act",
+    "get_activity_at_injection",
+    "perc_diff",
+    "tew_scatt",
+    "GammaCamera",
+    "ewin_montage",
+    "plot_tac_residuals",
+    "rtst_to_mask",
+    "monoexp_fun",
+    "biexp_fun",
+    "triexp_fun",
+    "DicomModify",
 ]
