890e55bec384d9093f2d7f16245fd3f1e203d1dd
sarakurkowska <sarakurkowska95@gmail.com>
2025-11-26 12:16:48 -0800

    adjust x axis based on the highest value

--- pytheranostics/plots/plots.py
+++ pytheranostics/plots/plots.py
@@ -86,7 +86,7 @@ def plot_tac_residuals(
     else:
         weights = None
     # Generate x-values for plotting the fitted model starting from x=0
-    x_fit = numpy.linspace(0, x_data[-1] * 3, 500)
+    x_fit = numpy.linspace(0, x_data.max() * 3, 500)
     y_fit = result.eval(x=x_fit)
 
     # First subplot: Linear scale plot
@@ -96,7 +96,7 @@ def plot_tac_residuals(
     # Plot fitted model
     ax1.plot(x_fit, y_fit, color="red")
     ax1.set_xlim(left=0)  # Start x-axis from zero
-    ax1.set_xlim(right=x_data[-1] * 2)  # Start y-axis from zero
+    ax1.set_xlim(right=x_data.max() * 2)  # Start y-axis from zero
     ax1.set_ylim(bottom=0)  # Start y-axis from zero
     ax1.set_title(region)
     ax1.set_xlabel(x_label)
@@ -119,7 +119,7 @@ def plot_tac_residuals(
     # Plot fitted model
     ax2.plot(x_fit, y_fit, color="red")
     ax2.set_xlim(left=0)  # Start x-axis from zero
-    ax2.set_xlim(right=x_data[-1] * 2)  # Start y-axis from zero
+    ax2.set_xlim(right=x_data.max() * 2)  # Start y-axis from zero
     ax2.set_yscale("log")
     ax2.set_title(title_text)
     ax2.set_xlabel(x_label)
